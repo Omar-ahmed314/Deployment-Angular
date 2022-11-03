310a38ac36aca1b9d9800bd2193e515be293b417
Omar A.Rezk <62177195+Omar-ahmed314@users.noreply.github.com>
2022-11-03 15:13:58 +0200

    fixing bug at frontend url

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev2.us-east-1.elasticbeanstalk.com/',
+  apiHost: 'http://www.udagram-api-dev2222.us-east-1.elasticbeanstalk.com/',
 };
 
 /*
